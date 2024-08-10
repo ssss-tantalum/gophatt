0d2b8bf84d359fbb09aa9cff131d2bef9d53897e
ssss-tantalum <ssss.tantalum@gmail.com>
2024-08-10 06:47:05 +0000

    make darkMode value store in localStrage

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,13 @@
 import 'htmx.org'
 import Alpine from 'alpinejs'
+import { persist } from '@alpinejs/persist'
 
 window.Alpine = Alpine
+Alpine.plugin(persist)
 
 Alpine.store('darkMode', {
-  on: true,
-  toggle() {
+  on: Alpine.$persist(true).as('darkMode'),
+  toggle: function () {
     this.on = !this.on
   },
 })
